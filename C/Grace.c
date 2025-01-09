bad838f92714ffb30b7a4c2914ec294a4e517e82
batche <vatche@batche.com>
2025-01-09 09:13:47 +0100

    suite

--- C/Grace.c
+++ C/Grace.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
-    uselessasf
+    u
 */
 #define Q 10
 #define S 11
-#define FT()int main(){char *s="#include <stdio.h>%c/*%c    uselessasf%c*/%c#define Q 10%c#define S 11%c#define FT()int main(){char *s=%c%s%c;printf(s,10,10,10,10,10,10,34,s,34,10);}%cFT()";printf(s,10,10,10,10,10,10,34,s,34,10);}
+#define FT()int main(){char *s="#include <stdio.h>%c/*%c    u%c*/%c#define Q 10%c#define S 11%c#define FT()int main(){char *s=%c%s%c;FILE *f = fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,s,10,10,10,10,10,10,34,s,34,34,34,34,34,10,10);fclose(f);}%cFT()%c";FILE *f = fopen("Grace_kid.c","w");fprintf(f,s,10,10,10,10,10,10,34,s,34,34,34,34,34,10,10);fclose(f);}
 FT()
